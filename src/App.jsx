a3a527c93a91d888e82dc4c7c9fe84808f5cc714
Lubeth <lubethjimenez@yahoo.com>
2026-05-06 21:04:24 -0400

    Add useState in parent App.jsx component & pass props between loginData, handleChange

--- src/App.jsx
+++ src/App.jsx
@@ -5,27 +5,27 @@ import LoginForm from './components/LoginForm'
 
 function App() {
 //Login Form Data object to store all inputs//FORM STATE//
-    const [loginData, setLoginData] = useState({
-      email: "",
-      name: "",
-      password: "",
-      age: "",
-    });
+  const [loginData, setLoginData] = useState({
+    email: "",
+    name: "",
+    password: "",
+    age: "",
+  });
 
 //FORM state UPDATE//
 //Handle Input Changes//
-function handleChange(event){
+  function handleChange(event) {
     const { name, value } = event.target;
-    console.log(name,value);
-    setLoginData({ ...loginData, [name]: value})
-};
+    console.log(name, value);
+    setLoginData({ ...loginData, [name]: value });
+  }
 
   return (
     <>
       <h1>React Form Day 3 Assignment</h1>
-      <LoginForm loginData={loginData} handleChange={handleChange}/>
+      <LoginForm loginData={loginData} handleChange={handleChange} />
     </>
-  )
+  );
 }
 
 export default App
--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -4,24 +4,20 @@ import React from 'react'
 function LoginForm({ loginData, handleChange }) {
   return (
     <div>
-        <h2>Please Login</h2>
+      <h2>Please Login</h2>
       <form>
-        <label htmlFor="email">
-          Email:
+        <label htmlFor="email">Email:
           <input type="email" name="email" id="email" value={loginData.email} onChange={handleChange}/>
         </label>
-        <label htmlFor="username">
-          Username:
+        <label htmlFor="username">Username:
           <input type="text" name="username" id="username" value={loginData.username} 
           onChange={handleChange}/>
         </label>
-        <label htmlFor="password">
-          Password:
+        <label htmlFor="password">Password:
           <input type="password" name="password" id="password" value={loginData.password} 
           onChange={handleChange}/>
         </label>
-        <label htmlFor="age">
-          Age:
+        <label htmlFor="age">Age:
           <input type="number" name="age" id="age" value={loginData.age} onChange={handleChange}/>
         </label>
         <button>Submit!</button>
