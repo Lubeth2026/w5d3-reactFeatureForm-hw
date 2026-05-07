814b7d461f206f5ad72c3f4c8145a2f30a951952
Lubeth <lubethjimenez@yahoo.com>
2026-05-06 20:54:50 -0400

    Add LoginForm component to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
 
 import './App.css'
+import { useState } from 'react'
+import LoginForm from './components/LoginForm'
 
 function App() {
-  
+//Login Form Data object to store all inputs//FORM STATE//
+    const [loginData, setLoginData] = useState({
+      email: "",
+      name: "",
+      password: "",
+      age: "",
+    });
+
+//FORM state UPDATE//
+//Handle Input Changes//
+function handleChange(event){
+    const { name, value } = event.target;
+    console.log(name,value);
+    setLoginData({ ...loginData, [name]: value})
+};
 
   return (
     <>
-      <h1>React From Day 3 Assignment</h1>
+      <h1>React Form Day 3 Assignment</h1>
+      <LoginForm loginData={loginData} handleChange={handleChange}/>
     </>
   )
 }
---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,33 @@
+
+import React from 'react'
+
+function LoginForm({ loginData, handleChange }) {
+  return (
+    <div>
+        <h2>Please Login</h2>
+      <form>
+        <label htmlFor="email">
+          Email:
+          <input type="email" name="email" id="email" value={loginData.email} onChange={handleChange}/>
+        </label>
+        <label htmlFor="username">
+          Username:
+          <input type="text" name="username" id="username" value={loginData.username} 
+          onChange={handleChange}/>
+        </label>
+        <label htmlFor="password">
+          Password:
+          <input type="password" name="password" id="password" value={loginData.password} 
+          onChange={handleChange}/>
+        </label>
+        <label htmlFor="age">
+          Age:
+          <input type="number" name="age" id="age" value={loginData.age} onChange={handleChange}/>
+        </label>
+        <button>Submit!</button>
+      </form>
+    </div>
+  );
+}
+
+export default LoginForm
